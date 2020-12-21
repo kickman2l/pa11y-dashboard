e56cab2cfeab6c0d07edbfc4e2552392d8c195c4
Serhii N <>
2020-12-21 15:07:20 +0200

    Security changes
    Url checking was added

--- route/new.js
+++ route/new.js
@@ -16,81 +16,97 @@
 
 const getStandards = require('../data/standards');
 const httpHeaders = require('http-headers');
+const parseUrl = require('parse-url');
+const compareUrls = require('compare-urls');
+const domains = require('../config/domains.json');
+const _ = require('underscore');
 
 module.exports = route;
 
 // Route definition
 function route(app) {
+    app.express.get('/new', (request, response) => {
+        const standards = getStandards().map((standard) => {
+            if (standard.title === 'WCAG2AA') {
+                standard.selected = true;
+            }
+            return standard;
+        });
+        response.render('new', {
+            standards: standards,
+            isNewTaskPage: true,
+        });
+    });
 
-	app.express.get('/new', (request, response) => {
-		const standards = getStandards().map(standard => {
-			if (standard.title === 'WCAG2AA') {
-				standard.selected = true;
-			}
-			return standard;
-		});
-		response.render('new', {
-			standards: standards,
-			isNewTaskPage: true
-		});
-	});
+    app.express.post('/new', (request, response) => {
+        let parsedActions;
+        if (request.body.actions) {
+            parsedActions = request.body.actions
+                .split(/[\r\n]+/)
+                .map((action) => {
+                    return action.trim();
+                })
+                .filter((action) => {
+                    return Boolean(action);
+                });
+        }
 
-	app.express.post('/new', (request, response) => {
+        let parsedHeaders;
+        if (request.body.headers) {
+            parsedHeaders = httpHeaders(request.body.headers, true);
+        }
 
-		let parsedActions;
-		if (request.body.actions) {
-			parsedActions = request.body.actions.split(/[\r\n]+/)
-				.map(action => {
-					return action.trim();
-				})
-				.filter(action => {
-					return Boolean(action);
-				});
-		}
+        const destinationUrl = parseUrl(request.body.url);
+        const isAllowed = _.find(domains.allowed, function (url) {
+            return compareUrls(url, destinationUrl.resource);
+        });
 
-		let parsedHeaders;
-		if (request.body.headers) {
-			parsedHeaders = httpHeaders(request.body.headers, true);
-		}
+        console.log(isAllowed, destinationUrl, domains);
 
-		const newTask = {
-			name: request.body.name,
-			url: request.body.url,
-			standard: request.body.standard,
-			ignore: request.body.ignore || [],
-			timeout: request.body.timeout || undefined,
-			wait: request.body.wait || undefined,
-			actions: parsedActions,
-			username: request.body.username || undefined,
-			password: request.body.password || undefined,
-			headers: parsedHeaders,
-			hideElements: request.body.hideElements || undefined
-		};
+        if (isAllowed === undefined) {
+            return response.render('new', {
+                error: 'Not allowed url',
+                task: {},
+            });
+        }
 
-		app.webservice.tasks.create(newTask, (error, task) => {
-			if (error) {
-				const standards = getStandards().map(standard => {
-					if (standard.title === newTask.standard) {
-						standard.selected = true;
-					}
-					standard.rules = standard.rules.map(rule => {
-						if (newTask.ignore.indexOf(rule.name) !== -1) {
-							rule.ignored = true;
-						}
-						return rule;
-					});
-					return standard;
-				});
-				newTask.actions = request.body.actions;
-				newTask.headers = request.body.headers;
-				return response.render('new', {
-					error: error,
-					standards: standards,
-					task: newTask
-				});
-			}
-			response.redirect(`/${task.id}?added`);
-		});
-	});
+        const newTask = {
+            name: request.body.name,
+            url: request.body.url,
+            standard: request.body.standard,
+            ignore: request.body.ignore || [],
+            timeout: request.body.timeout || undefined,
+            wait: request.body.wait || undefined,
+            actions: parsedActions,
+            username: request.body.username || undefined,
+            password: request.body.password || undefined,
+            headers: parsedHeaders,
+            hideElements: request.body.hideElements || undefined,
+        };
 
+        app.webservice.tasks.create(newTask, (error, task) => {
+            if (error) {
+                const standards = getStandards().map((standard) => {
+                    if (standard.title === newTask.standard) {
+                        standard.selected = true;
+                    }
+                    standard.rules = standard.rules.map((rule) => {
+                        if (newTask.ignore.indexOf(rule.name) !== -1) {
+                            rule.ignored = true;
+                        }
+                        return rule;
+                    });
+                    return standard;
+                });
+                newTask.actions = request.body.actions;
+                newTask.headers = request.body.headers;
+                return response.render('new', {
+                    error: error,
+                    standards: standards,
+                    task: newTask,
+                });
+            }
+            response.redirect(`/${task.id}?added`);
+        });
+    });
 }
